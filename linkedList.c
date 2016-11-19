96cd3c48334b1afd94ec5c0334e0d208fc8c2205
wllm <wnunez.mx@gmail.com>
2016-11-18 23:13:16 -0600

    added functions to delete the first and the last item of the list

--- linkedList.c
+++ linkedList.c
@@ -6,21 +6,56 @@ typedef struct node {
 } node_t;
 void print_list(node_t * head);
 void push(node_t * head, int val);
+int pop(node_t ** head);
+int remove_last(node_t * head);
 
 void main(){
 
-node_t * head = NULL;
-head = malloc(sizeof(node_t));
+	node_t * head = NULL;
+	head = malloc(sizeof(node_t));
 
-head->val =0; 
-head->next = NULL;
-int i=1;
-for(i;i<10;i++){
-push(head,i);
-print_list(head); 
-}   
+	head->val =0; 
+	head->next = NULL;
+	int i=1;
+	for(i;i<10;i++){
+		push(head,i);
+		print_list(head); 
+		}   
+    }
+int remove_last(node_t * head) {
+    int retval = 0;
+    /* if there is only one item in the list, remove it */
+    if (head->next == NULL) {
+        retval = head->val;
+        free(head);
+        return retval;
+    }
+    /* get to the last node in the list */
+    node_t * current = head;
+    while (current->next->next != NULL) {
+        current = current->next;
+    }
+    /* now current points to the last item of the list, so let's remove current->next */
+    retval = current->next->val;
+    free(current->next);
+    current->next = NULL;
+    return retval;
 }
+int pop(node_t ** head) {
+    int retval = -1;
+    node_t * next_node = NULL;
 
+    if (*head == NULL) {
+        return -1;
+    }
+
+    next_node = (*head)->next;
+    retval = (*head)->val;
+    free(*head);
+    *head = next_node;
+
+    return retval;
+}
 void push(node_t * head, int val) {
     node_t * current = head;
     while (current->next != NULL) {

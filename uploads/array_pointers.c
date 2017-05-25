bbf2f6756c1ab4332b39e410dc4086ee70b37b89
William Enrique Nunez Chavez <wnunez.mx@gmail.com>
2017-05-24 20:31:07 -0500

    Create array_pointers.c
    experiments to watch out if the syntax to access an array could change and still work.

---
+++ uploads/array_pointers.c
@@ -0,0 +1,10 @@
+//breakingcodemx/array_pointers.c
+#include<stdio.h>
+int main() 
+{
+    int arr[10];
+    arr[0] = 1;
+	printf("%d\n", arr[0] );
+    printf("%d\n", 0[arr] );     
+    return 0;    
+}
